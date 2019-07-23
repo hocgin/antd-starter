ed47ed5a97b65a37b57e3a2d7fff90048aadc33d
hcjin <hocgin@gmail.com>
2019-07-23 08:51:45 +0800

    基础结构 #添加常用配置及工具类

---
+++ src/utils/Config.js
@@ -0,0 +1,29 @@
+export default class Config {
+  /**
+   * 默认配置
+   * @type {{}}
+   */
+  static defaultConfig = {
+    // 请求地址
+    host: "http://127.0.0.1:8080"
+  };
+
+  /**
+   * 自定义配置
+   * @returns {{}}
+   */
+  static getCustomConfig() {
+    return {};
+  }
+
+  /**
+   * 当前配置
+   * @returns {Config.defaultConfig}
+   */
+  static getConfigs() {
+    return {
+      ...Config.defaultConfig,
+      ...Config.getCustomConfig(),
+    };
+  }
+}
