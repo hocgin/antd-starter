babc024ef43e2e1ba8144d36d3a5158336b8d785
hocgin <hocgin@gmail.com>
2019-12-26 09:14:33 +0800

    TPL

--- src/pages/.tpl/index.js
+++ src/pages/.tpl/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import styles from './index.less';
 
 class index extends React.Component {
+
+  componentDidMount() {
+    // window.addEventListener('resize', this.handleResize);
+  }
+
+  componentWillUnmount() {
+    // window.removeEventListener('resize', this.handleResize);
+  }
+
+
   render() {
     let {} = this.props;
     return (
