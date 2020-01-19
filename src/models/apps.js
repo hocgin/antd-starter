726980c8cc9067013fd86b5728304647be25b892
hocgin <hocgin@gmail.com>
2020-01-19 12:57:48 +0800

    TPL

--- src/models/apps.js
+++ src/models/apps.js
@@ -2,17 +2,15 @@ import qs from "query-string";
 
 export default {
   namespace: 'app',
-  state: {
-  },
-  effects: {
-  },
-  reducers: {
-  },
   subscriptions: {
     setup({ dispatch, history }, done) {
       return history.listen(({ pathname, search }) => {
         const query = qs.parse(search);
         switch (pathname) {
+          case '/':{
+            // TODO
+            break;
+          }
           default:{
             console.log(pathname);
           }
