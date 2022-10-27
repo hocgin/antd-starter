27bb528627dce1ddad353b4f5833ed5f6381672d
hocgin <hocgin@gmail.com>
2022-10-27 18:04:37 +0800

    [新增功能](develop): 调整

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,5 @@
+import { Redirect } from '@hocgin/ui';
+
 export default () => {
-  return <div>地址打错了你知道嘛</div>;
+  return <Redirect redirectUrl={'https://cdn.hocgin.top/404'}>地址打错了你知道嘛</Redirect>;
 };
