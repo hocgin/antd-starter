02a10a97956469b2281f02af4bd71f255407e5b8
hocgin <hocgin@gmail.com>
2022-12-05 22:00:11 +0800

    [新增功能](develop): 调整

--- src/layouts/BasicLayout/index.tsx
+++ src/layouts/BasicLayout/index.tsx
@@ -1,12 +1,13 @@
 import styles from './index.less';
 import React from 'react';
 import { Outlet } from 'umi';
+import { Footer } from '@hocgin/ui';
 
 const BasicLayout: React.FC<{}> = () => {
   return (<div className={styles.normal}>
     <h1 className={styles.title}>Yay! Welcome to umi!</h1>
     <Outlet />
-    {/*<Footer />*/}
+    <Footer />
   </div>);
 };
 export default BasicLayout;
