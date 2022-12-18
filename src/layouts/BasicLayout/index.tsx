af83ff16843a5d883a438771891847468113c00e
hocgin <hocgin@gmail.com>
2022-12-18 21:24:19 +0800

    [新增功能](develop): 调整

--- src/layouts/BasicLayout/index.tsx
+++ src/layouts/BasicLayout/index.tsx
@@ -1,12 +1,16 @@
 import styles from './index.less';
 import React from 'react';
-import {Footer} from "@hocgin/ui";
+import { default as theme } from '@/theme';
+import { Footer } from '@hocgin/ui';
+import { ConfigProvider } from 'antd';
 
-const BasicLayout: React.FC<{}> = (props, ref) => {
-  return (<div className={styles.normal}>
-    <h1 className={styles.title}>Yay! Welcome to umi!</h1>
-    {props.children}
-    <Footer />
-  </div>);
+const BasicLayout: React.FC<{ children: any }> = ({ children }) => {
+  return (<ConfigProvider theme={{ token: theme }}>
+    <div className={styles.normal}>
+      <h1 className={styles.title}>Yay! Welcome to umi!</h1>
+      {children}
+      <Footer />
+    </div>
+  </ConfigProvider>);
 };
 export default BasicLayout;
