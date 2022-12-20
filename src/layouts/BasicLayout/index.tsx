2193b038c2486f5134a946fc8c8193a76f460029
hocgin <hocgin@gmail.com>
2022-12-20 21:58:57 +0800

    [新增功能](develop): 调整

--- src/layouts/BasicLayout/index.tsx
+++ src/layouts/BasicLayout/index.tsx
@@ -3,9 +3,10 @@ import React from 'react';
 import { default as theme } from '@/theme';
 import { Footer } from '@hocgin/ui';
 import { ConfigProvider } from 'antd';
+import { ANT_PREFIX_CLS } from '../../../config/antd.prefix.cls';
 
 const BasicLayout: React.FC<{ children: any }> = ({ children }) => {
-  return (<ConfigProvider theme={{ token: theme }}>
+  return (<ConfigProvider prefixCls={ANT_PREFIX_CLS} theme={{ token: theme }}>
     <div className={styles.normal}>
       <h1 className={styles.title}>Yay! Welcome to umi!</h1>
       {children}
