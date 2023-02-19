9e462e0fef6f335c4063efff7b5c8a9b41f0f516
hocgin <hocgin@gmail.com>
2023-02-20 02:45:04 +0800

    [新增功能](develop): 调整

--- src/components/StoreLink/index.tsx
+++ src/components/StoreLink/index.tsx
@@ -1,27 +1,17 @@
 import React from 'react';
 import { Tag } from 'antd';
-import { PlatformKit } from '@hocgin/hkit';
-import { WebExtension } from '@hocgin/browser-addone-kit';
+import { i18nKit, WebExtension } from '@hocgin/browser-addone-kit';
 
 export const StoreLink: React.FC<{
   className?: string;
 }> = (props) => {
-  let url = PlatformKit.isEdge
-    ? 'https://microsoftedge.microsoft.com/addons/search/hocgin'
-    : 'https://chrome.google.com/webstore/search/hocgin';
   return (
     <Tag
       color="#D25643"
       style={{ cursor: 'pointer' }}
-      onClick={() => {
-        if (WebExtension?.tabs) {
-          WebExtension.tabs.create({ url: url });
-        } else if (window?.open) {
-          window.open(url, '_blank');
-        }
-      }}
+      onClick={WebExtension.kit.openRecommendURL}
     >
-      推荐
+      {i18nKit.getMessage('recommend' as any)}
     </Tag>
   );
 };
