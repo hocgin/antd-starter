5a00d446ae4c53f40abf620ab75eefe4685fd878
hocgin <hocgin@gmail.com>
2023-02-23 22:55:53 +0800

    [新增功能](develop): 调整

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
-import { getAllLocales } from 'umi';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import '@/request.config';
+import 'dayjs/locale/zh-cn';
+import 'dayjs/locale/zh-tw';
+import 'dayjs/locale/en';
+import { I18nKit } from '@hocgin/browser-addone-kit';
 
 dayjs.extend(relativeTime);
-
-// 国际化配置
-getAllLocales().forEach((locale) => {
-});
+dayjs.locale(I18nKit.getMessageOrDefault('lang', 'en'));
 
 // 全局状态配置
 export async function getInitialState() {
